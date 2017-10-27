ffe310836a451e1d1a3bff285ddf74b322d9a25b
Алексей Челышев <alexey.chelyshev.pvm@gmail.com>
2017-10-27 19:00:10 +0300

    улучшил внешний вид приложения;

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 function fun1() {
     var rtl = document.getElementById("rtl").value;
-    var ttl = document.getElementById("ttl");
-    var rtr = document.getElementById("rtr").value;
-    var ttr = document.getElementById("ttr");
     var rbl = document.getElementById("rbl").value;
+    var rtr = document.getElementById("rtr").value;
+    var rbr = document.getElementById("rbr").value;  // инициализация полос прокрутки и текстовых полей
+    var ttr = document.getElementById("ttr");
+    var ttl = document.getElementById("ttl");
     var tbl = document.getElementById("tbl");
-    var rbr = document.getElementById("rbr").value;
     var tbr = document.getElementById("tbr");
 
-    var block = document.getElementById("block");
+    var block = document.getElementById("block"); // инициализация редактируемого объекта
 
     ttl.value = rtl;
     ttr.value = rtr;
-    tbl.value = rbl;
+    tbl.value = rbl; //тексту в текстовых полях задается значение из полос прокрутки
     tbr.value = rbr;
 
 
 
-    block.style.borderRadius = rtl + 'px ' +rtr + 'px '  +rbr + 'px ' + rbl +'px ' ;
+    block.style.borderRadius = rtl + 'px ' +rtr + 'px '  +rbr + 'px ' + rbl +'px ' ; // применение значений  из полос
+                                                                                                    // прокрустки к редактируемому объекту
 }
